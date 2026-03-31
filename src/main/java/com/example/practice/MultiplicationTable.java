c77746a1db1c81d2322c75552d874f646aeaa3d6
tanmay sati <ts6880@srmist.edu.in>
2026-03-31 18:53:39 +0530

    Added practice program NumberCheck

---
+++ src/main/java/com/example/practice/MultiplicationTable.java
@@ -0,0 +1,15 @@
+package com.example.practice;
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " * " + i + " = " + (number * i));
+        }
+    }
+}
---
+++ src/main/java/com/example/practice/NumberCheck.java
@@ -0,0 +1,31 @@
+package com.example.practice;
+import java.util.Scanner;
+
+public class NumberCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+
+        System.out.println("Enter 5 numbers:");
+
+        for (int i = 0; i < 5; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < 5; i++) {
+            int num = arr[i];
+
+            if (num > 0) {
+                if (num % 2 == 0) {
+                    System.out.println(num + " is positive and even");
+                } else {
+                    System.out.println(num + " is positive and odd");
+                }
+            } else if (num < 0) {
+                System.out.println(num + " is negative");
+            } else {
+                System.out.println(num + " is zero");
+            }
+        }
+    }
+}
