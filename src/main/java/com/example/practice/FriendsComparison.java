39d2516048d0bb79ae102cece1238d15c2b045c6
tanmay sati <ts6880@srmist.edu.in>
2026-04-03 11:18:23 +0530

    Added Java program: FriendsComparison

---
+++ src/main/java/com/example/practice/FriendsComparison.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class FriendsComparison {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] names = {"Amar", "Akbar", "Anthony"};
+        int[] age = new int[3];
+        double[] height = new double[3];
+
+        // Input
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Enter age of " + names[i] + ": ");
+            age[i] = sc.nextInt();
+
+            System.out.print("Enter height of " + names[i] + ": ");
+            height[i] = sc.nextDouble();
+        }
+
+        // Find youngest
+        int youngestIndex = 0;
+        for (int i = 1; i < 3; i++) {
+            if (age[i] < age[youngestIndex]) {
+                youngestIndex = i;
+            }
+        }
+
+        // Find tallest
+        int tallestIndex = 0;
+        for (int i = 1; i < 3; i++) {
+            if (height[i] > height[tallestIndex]) {
+                tallestIndex = i;
+            }
+        }
+
+        // Output
+        System.out.println("Youngest: " + names[youngestIndex]);
+        System.out.println("Tallest: " + names[tallestIndex]);
+
+        sc.close();
+    }
+}
